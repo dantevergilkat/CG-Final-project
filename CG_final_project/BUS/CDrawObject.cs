204cc3f422c4721227b9107632fcc03975525d1b
tienhaophung <tienhaophung@gmail.com>
2018-12-06 23:53:16 +0700

    Push project

---
+++ CG_final_project/BUS/CDrawObject.cs
@@ -0,0 +1,146 @@
+﻿using SharpGL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BUS
+{
+	public class CDrawObject
+	{
+		
+		public void drawColouredCube(OpenGL gl)
+		{
+			// Ve hinh lap phuong voi canh a bat ky
+			float a = 2.0f;
+			gl.Begin(OpenGL.GL_QUADS);
+
+			gl.Color(0.0f, 1.0f, 0.0f);    // Color Blue
+			gl.Vertex(0.0f, 0.0f, 0.0f);    // Top Right Of The Quad (Top)
+			gl.Vertex(a, 0.0f, 0.0f);    // Top Left Of The Quad (Top)
+			gl.Vertex(a, 0.0f, a);    // Bottom Left Of The Quad (Top)
+			gl.Vertex(0.0f, 0.0f, a);    // Bottom Right Of The Quad (Top)
+
+			gl.Color(1.0f, 0.5f, 0.0f);    // Color Orange
+			gl.Vertex(0.0f, 0.0f, 0.0f);    // Top Right Of The Quad (Bottom)
+			gl.Vertex(0.0f, a, 0.0f);    // Top Left Of The Quad (Bottom)
+			gl.Vertex(a, a, 0.0f);    // Bottom Left Of The Quad (Bottom)
+			gl.Vertex(a, 0.0f, 0.0f);    // Bottom Right Of The Quad (Bottom)
+
+			gl.Color(1.0f, 0.0f, 0.0f);    // Color Red    
+			gl.Vertex(0.0f, 0.0f, 0.0f);    // Top Right Of The Quad (Front)
+			gl.Vertex(0.0f, a, 0.0f);    // Top Left Of The Quad (Front)
+			gl.Vertex(0.0f, a, a);    // Bottom Left Of The Quad (Front)
+			gl.Vertex(0.0f, 0.0f, a);    // Bottom Right Of The Quad (Front)
+
+			gl.Color(1.0f, 1.0f, 0.0f);    // Color Yellow
+			gl.Vertex(0.0f, a, a);    // Top Right Of The Quad (Back)
+			gl.Vertex(a, a, a);    // Top Left Of The Quad (Back)
+			gl.Vertex(a, 0.0f, a);    // Bottom Left Of The Quad (Back)
+			gl.Vertex(0.0f, 0.0f, a);    // Bottom Right Of The Quad (Back)
+
+			gl.Color(0.0f, 0.0f, 1.0f);    // Color Blue
+			gl.Vertex(a, a, a);    // Top Right Of The Quad (Left)
+			gl.Vertex(a, a, 0.0f);    // Top Left Of The Quad (Left)
+			gl.Vertex(a, 0.0f, 0.0f);    // Bottom Left Of The Quad (Left)
+			gl.Vertex(a, 0.0f, a);    // Bottom Right Of The Quad (Left)
+
+			gl.Color(1.0f, 0.0f, 1.0f);    // Color Violet
+			gl.Vertex(0.0f, a, 0.0f);    // Top Right Of The Quad (Right)
+			gl.Vertex(a, a, 0.0f);    // Top Left Of The Quad (Right)
+			gl.Vertex(a, a, a);    // Bottom Left Of The Quad (Right)
+			gl.Vertex(0.0f, a, a);    // Bottom 
+
+			gl.End();
+			gl.Flush();
+		}
+
+		// Ve hinh chop, day la hinh vuong
+		public void drawPyramidWithSpareBottom(OpenGL gl) {
+			float a = 2.0f;
+			// Ve hinh chop day hinh vuong voi dinh S tuy y va canh day la a
+			// Gia su S(10, 6, 4)
+			float h = 5;
+
+			// Ve day
+			gl.Begin(OpenGL.GL_QUADS);
+			gl.Color(0.0f, 1.0f, 0.0f); // Blue
+			gl.Vertex(0.0f, 0.0f, 0.0f);
+			gl.Vertex(a, 0.0f, 0.0f);
+			gl.Vertex(a, 0.0f, a);
+			gl.Vertex(0.0f, 0.0f, a);
+			gl.End();
+			gl.Flush();
+
+			// Ve 4 mat ben
+			gl.Begin(OpenGL.GL_TRIANGLES);
+			// Ve mat ben trai SAB
+			gl.Color(1.0f, 0.5f, 0.0f);    // Color Orange
+			gl.Vertex(3.0f, 6.0f, 2.0f);
+			gl.Vertex(0.0f, 0.0f, 0.0f);
+			gl.Vertex(0.0f, 0.0f, a);
+
+			// Ve mat giua SBC
+			gl.Color(1.0f, 0.0f, 0.0f);    // Color Red   
+			gl.Vertex(3.0f, 6.0f, 2.0f);
+			gl.Vertex(0.0f, 0.0f, a);
+			gl.Vertex(a, 0.0f, a);
+
+			// Ve mat ben phai SCD
+			gl.Color(1.0f, 1.0f, 0.0f);    // Color Yellow
+			gl.Vertex(3.0f, 6.0f, 2.0f);
+			gl.Vertex(a, 0.0f, a);
+			gl.Vertex(a, 0.0f, 0.0f);
+
+			// Ve mat sau SAD
+			gl.Color(1.0f, 0.0f, 1.0f);    // Color Violet
+			gl.Vertex(3.0f, 6.0f, 2.0f);
+			gl.Vertex(0.0f, 0.0f, 0.0f);
+			gl.Vertex(a, 0.0f, 0.0f);
+
+			gl.End();
+			gl.Flush();
+		}
+
+		public void drawCylinderWithTriangleBottom(OpenGL gl) {
+			// Ve hinh tru co day la tam giac deu voi canh a tuy y
+			
+			float a = 2.0f;
+			float h = 5.0f;
+
+			gl.Begin(OpenGL.GL_TRIANGLES);
+			// Ve day ABC
+			gl.Color(0.0f, 1.0f, 0.0f); // Blue
+			gl.Vertex(0.0f, 0.0f, 0.0f);
+			gl.Vertex(a, 0.0f, 0.0f);
+			gl.Vertex(a / 2, 0.0f, a);
+			gl.End();
+			gl.Flush();
+
+			// Ve cac mat ben
+			gl.Begin(OpenGL.GL_QUADS);
+			// Ve mat ben thu nhat AA'C'C
+			gl.Color(1.0f, 0.5f, 0.0f);    // Color Orange
+			gl.Vertex(0.0f, 0.0f, 0.0f);
+			gl.Vertex(0.0f, h, 0.0f);
+			gl.Vertex(a / 2, h, a);
+			gl.Vertex(a / 2, 0.0f, a);
+
+			// Ve mat ben thu hai CC'B'B
+			gl.Color(1.0f, 0.0f, 0.0f);    // Color Red  
+			gl.Vertex(a / 2, 0.0f, a);
+			gl.Vertex(a / 2, h, a);
+			gl.Vertex(a, h, 0.0f);
+			gl.Vertex(a, 0.0f, 0.0f);
+
+			// Ve mat sau AA'B'B
+			gl.Color(1.0f, 1.0f, 0.0f);    // Color Yellow
+			gl.Vertex(0.0f, 0.0f, 0.0f);
+			gl.Vertex(0.0f, h, 0.0f);
+			gl.Vertex(a, h, 0.0f);
+			gl.Vertex(a, 0.0f, 0.0f);
+		}
+	}
+}
---
+++ CG_final_project/GUI/Form1.cs
@@ -0,0 +1,76 @@
+﻿using SharpGL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BUS;
+
+namespace GUI
+{
+	public partial class Form1 : Form
+	{
+		// Khoi tao doi tuong lop CDrawObject de ve
+		CDrawObject drObj = new CDrawObject();
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void openGLControl1_OpenGLDraw(object sender, SharpGL.RenderEventArgs args)
+		{
+			OpenGL gl = openGLControl1.OpenGL;
+			// Clear vung nho dem
+			gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+
+			// Draw a coloured cube
+			//drObj.drawColouredCube(gl);
+
+			// Draw pyramid with square bottom
+			//drObj.drawPyramidWithSpareBottom(gl);
+
+			// Draw cylinder with triangle bottom
+			drObj.drawCylinderWithTriangleBottom(gl);
+		}
+
+		private void openGLControl1_OpenGLInitialized(object sender, EventArgs e)
+		{
+			OpenGL gl = openGLControl1.OpenGL;
+			// Set man hinh OpenGL la den
+			gl.ClearColor(0, 0, 0, 0);
+		}
+
+		private void openGLControl1_Resized(object sender, EventArgs e)
+		{
+			OpenGL gl = openGLControl1.OpenGL;
+
+			//set ma tran viewport
+			// chiem toan bo cua so cua OpenGL
+			gl.Viewport(
+				0, 0,
+				openGLControl1.Width,
+				openGLControl1.Height);
+
+			//set ma tran phep chieu
+			gl.MatrixMode(OpenGL.GL_PROJECTION);
+			// Nhan voi ma tran don vi
+			gl.LoadIdentity();
+			// Chieu phoi canh
+			gl.Perspective(60,
+			openGLControl1.Width / openGLControl1.Height,
+				1.0, 20.0);
+	
+			//set ma tran model view
+			gl.MatrixMode(OpenGL.GL_MODELVIEW);
+			gl.LookAt(
+				5, 7, 6,
+				0, 0, 0,
+				0, 1, 0);
+
+		}
+	}
+}
